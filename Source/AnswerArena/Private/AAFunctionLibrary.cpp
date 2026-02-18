8d6a5e4fa30f6326c5c87c135ff039f2a95eb5ba
Hades <2077049069@qq.com>
2026-02-18 19:14:39 +0800

    显示两个数的运算

---
+++ Source/AnswerArena/Private/AAFunctionLibrary.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AAFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/AAPlayerState.h"
+#include "UI/HUD/AAHUD.h"
+#include "UI/WidgetController/AAWidgetController.h"
+
+UOverlayWidgetController* UAAFunctionLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAAHUD* AuraHUD = Cast<AAAHUD>(PC->GetHUD()))
+		{
+			AAAPlayerState* PS = PC->GetPlayerState<AAAPlayerState>();
+			const FWidgetControllerParams WidgetControllerParams (PC, PS);
+			return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
+
+UCalculatorWidgetController* UAAFunctionLibrary::GetCalculatorWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAAHUD* AuraHUD = Cast<AAAHUD>(PC->GetHUD()))
+		{
+			AAAPlayerState* PS = PC->GetPlayerState<AAAPlayerState>();
+			const FWidgetControllerParams WidgetControllerParams (PC, PS);
+			return AuraHUD->GetCalculatorWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
